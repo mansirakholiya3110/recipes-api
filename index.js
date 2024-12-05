0c04801c108bfddbbc5d1fd3b647cec2a2be4183
Mansi Rakholiya <mansirakholiya570@gmail.com>
2024-12-05 17:27:09 +0530

    port

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const recipeRoute = require("./routes/recipe.route")
 require("dotenv").config()
 
 const app = express()
-const port = process.env.PORT || 3000
-const DBConnectionString = process.env.MONGODB_URI
+
+
+const port = process.env.PORT || 9099
+const dbURL = process.env.MONGO_URL;
 
 // Middleware
 app.use(express.json())
@@ -16,13 +18,10 @@ app.get("/", (req, res) => {
     res.send("This is from node API")
 })
 
-app.listen(port, () => {
-    console.log("Server is running on port 3000")
-})
 
-mongoose.connect(DBConnectionString)
-.then(() => {
-    console.log("Connected to database !")
-}).catch(() => {
-    console.log("Connection failed")
+app.listen(port, () => {
+    mongoose.connect(dbURL)
+        .then(() => console.log(`DB connecter...!!`))
+        .catch((err) => console.log(err));
+    console.log(`server start..http://localhost:${port}`);
 })
